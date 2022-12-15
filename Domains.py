c454f8f50f451315c86befe70067ca2f2ac48a25
Prine4 <czerwinski180@gmail.com>
2022-12-15 22:22:11 +0100

    Added input and file creation

--- Domains.py
+++ Domains.py
@@ -4,16 +4,20 @@ import requests
 class Domens:
 
     def check_domens(ras):
+        file = open('output.csv', 'x')
         for ele in ras:
             try:
                 response = requests.get('http://' + ele)
                 if response.status_code == 200:
+                    file.write(ele + ',')
                     print(ele)
             except OSError:
                 pass
 
     def geting_domens_from_file():
-        file = open('d1', 'r').readlines()
+        print('Tape name of the file with domains:')
+        name_of_file = input()
+        file = open(name_of_file, 'r').readlines()
         ras = []
         for line in file:
             ras.append(line.strip())
